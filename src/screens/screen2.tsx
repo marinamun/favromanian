43c2b84cbfa36f4c9d965d3cc077e811a8ea9444
Marina Muñoz <marinacerejidoo@gmail.com>
2025-09-10 22:54:50 +0200

    added confetti effect

--- src/screens/screen2.tsx
+++ src/screens/screen2.tsx
@@ -1,9 +1,11 @@
 import adult from "../../public/adult.png";
+import Fireworks from "react-canvas-confetti/dist/presets/fireworks";
+
 const screen2 = () => {
   return (
     <>
       <div className="screen-container">
-        <h1>Happy birthday!!!</h1>
+        <Fireworks autorun={{ speed: 2 }} />;<h1>Happy birthday!!!</h1>
         <img src={adult} style={{ width: "60%" }} />
       </div>
     </>
